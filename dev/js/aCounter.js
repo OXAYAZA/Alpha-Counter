db8ecc540c0e39c5fc52c305ee9a668e328c74ed
oxayaza <gashishmarihuanovich@gmail.com>
2018-12-24 15:28:44 +0200

    UPD: αCounter v0.1.1

--- dev/js/aCounter.js
+++ dev/js/aCounter.js
@@ -2,15 +2,13 @@
 
 /**
  * @module   αCounter
- * @version  v0.1.0
+ * @version  v0.1.1
  * @author   OXAYAZA {@link https://github.com/OXAYAZA}
  * @license  CC BY-SA 4.0 {@link https://creativecommons.org/licenses/by-sa/4.0/}
  * @requires module:αUtil
  * @see      {@link https://github.com/OXAYAZA/Alpha-Counter}
  * @see      {@link https://codepen.io/OXAYAZA/pen/JJryqW}
  * @see      {@link https://www.linkedin.com/in/%D1%8E%D1%80%D0%B8%D0%B9-%D0%B1%D0%B0%D0%B7%D0%B0%D0%B5%D0%B2-166479165/}
- *
- * @todo внутренние параметры
  */
 function aCounter ( data ) {
 	function Counter ( data ) {
@@ -34,6 +32,15 @@ function aCounter ( data ) {
 		return this;
 	}
 
+	// Служебные параметры
+	Counter.prototype.internal = {
+		intervalId: null,
+		value:      0,
+		loops:      0,
+		increment:  0,
+		loop:       0
+	};
+
 	// Параметры по умолчанию
 	Counter.prototype.defaults = {
 		node:       null,
@@ -49,27 +56,27 @@ function aCounter ( data ) {
 
 	// Запуск счетчика
 	Counter.prototype.run = function () {
-		clearInterval( this.intervalId );
-		this.value     = this.params.from;
-		this.loops     = Math.ceil( this.params.duration / this.params.refresh );
-		this.increment = ( this.params.to - this.params.from) / this.loops;
-		this.loop      = 0;
-		if ( this.params.onStart instanceof Function ) this.params.onStart.call( this, ~~this.value );
-		this.intervalId = setInterval( this.update.bind( this ), this.params.refresh );
+		clearInterval( this.internal.intervalId );
+		this.internal.value     = this.params.from;
+		this.internal.loops     = Math.ceil( this.params.duration / this.params.refresh );
+		this.internal.increment = ( this.params.to - this.params.from) / this.internal.loops;
+		this.internal.loop      = 0;
+		if ( this.params.onStart instanceof Function ) this.params.onStart.call( this, ~~this.internal.value );
+		this.internal.intervalId = setInterval( this.update.bind( this ), this.params.refresh );
 	};
 
 	// обновление значения счетчика
 	Counter.prototype.update = function () {
-		this.value += this.increment;
-		this.loop++;
+		this.internal.value += this.internal.increment;
+		this.internal.loop++;
 
-		if ( this.params.onUpdate instanceof Function ) this.params.onUpdate.call( this, ~~this.value );
+		if ( this.params.onUpdate instanceof Function ) this.params.onUpdate.call( this, ~~this.internal.value );
 
-		if ( this.loop >= this.loops ) {
-			clearInterval( this.intervalId );
-			this.value = this.params.to;
+		if ( this.internal.loop >= this.internal.loops ) {
+			clearInterval( this.internal.intervalId );
+			this.internal.value = this.params.to;
 
-			if ( this.params.onComplete instanceof Function ) this.params.onComplete.call( this, ~~this.value );
+			if ( this.params.onComplete instanceof Function ) this.params.onComplete.call( this, ~~this.internal.value );
 		}
 
 		this.render();
@@ -78,9 +85,9 @@ function aCounter ( data ) {
 	// Отрисовка значения счетчика
 	Counter.prototype.render = function () {
 		if ( this.params.formatter instanceof Function ) {
-			this.params.node.innerHTML = this.params.formatter.call( this, ~~this.value );
+			this.params.node.innerHTML = this.params.formatter.call( this, ~~this.internal.value );
 		} else {
-			this.params.node.innerHTML = ~~this.value;
+			this.params.node.innerHTML = ~~this.internal.value;
 		}
 	};
 
